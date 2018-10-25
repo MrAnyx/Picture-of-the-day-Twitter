a33f7e23dbd57b34da58fd2ffb56f0f49068665a
MrAnyx <1ptitigre@gmail.com>
2018-10-25 22:57:06 +0200

    final commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const tokens = require('./tokens.json')
+const https = require('https')
+const fs = require('fs')
+const request = require('request');
+var Twitter = require('twitter')
+
+var client = new Twitter({
+  consumer_key: tokens.consumer_key,
+  consumer_secret: tokens.consumer_secret,
+  access_token_key: tokens.access_token,
+  access_token_secret: tokens.access_token_secret
+});
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+function tweet_apod(){
+  try{
+    https.get("https://api.nasa.gov/planetary/apod?api_key="+tokens.nasa_api, (resp) =>{
+      let data = '';
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+      resp.on('end', () => {
+        let apod = JSON.parse(data)
+        download(apod.hdurl, './images/image_apod.jpg', function(){
+          console.log(`L'image du ${apod.date} a été téléchargée avec succès`);
+          var image = fs.readFileSync("./images/image_apod.jpg");
+          client.post('media/upload', {media: image}, function(error, media, response) {
+            if (!error) {
+              var status = {
+                status: `The picture of the day (${apod.date}) : "${apod.title}" taken by ${apod.copyright}.\n\nFor more information, check out the @NASA website : https://apod.nasa.gov/apod/archivepix.html`,
+                media_ids: media.media_id_string // Pass the media id string
+              }
+              client.post('statuses/update', status, function(){
+                console.log(`L'image du ${apod.date} a été tweeté avec succès`);
+              });
+            }
+          });
+        })
+      });
+    })
+  }catch(e){
+    console.log(`Une erreur est survenue\n${e.message}`);
+  }
+}
+
+tweet_apod()
+setInterval(tweet_apod, 1000*60*60*24)
